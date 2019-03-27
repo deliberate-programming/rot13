fd93b36df9b1579fdbec11fcda981f898417089a
Ralf Westphal <info@ralfw.de>
2019-03-27 10:30:10 +0100

    <dp commit>

--- src/rot13/rot13/Config.cs
+++ src/rot13/rot13/Config.cs
@@ -1,8 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+
 namespace rot13
 {
     internal class Config
     {
+        private readonly string[] _args;
+
+        public enum Commands {
+            Encrypt,
+            Decrypt
+        }
+        
+        
         public Config(string[] args)
+        {
+            _args = args;
+        }
+
+
+        (Commands Command, IEnumerable<string> Sources) Parse()
         {
             
         }
