141ec04d0fd6c2e241ccf334d5a58dbfcb98ec26
Ralf Westphal <info@ralfw.de>
2019-03-27 10:08:10 +0100

    <dp commit>

--- src/rot13/rot13/FilesystemProvider.cs
+++ src/rot13/rot13/FilesystemProvider.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 namespace rot13
 {
@@ -8,6 +9,16 @@ namespace rot13
     {
         public IEnumerable<string> EnumerateSourcefile(IEnumerable<string> sources)
         {
+            var allFilenames = EnumerateAllFiles(sources);
+            return 
+        }
+
+        public string LoadText(string filename) => File.ReadAllText(filename);
+
+        public void  Store(string filename, string text)
+        {
+            // store
+            // delete original
             throw new NotImplementedException();
         }
     }
