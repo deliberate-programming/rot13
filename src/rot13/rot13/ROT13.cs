c844cb48469664c4a611ff82ba765c23c5039131
Ralf Westphal <info@ralfw.de>
2019-03-27 10:02:10 +0100

    <dp commit>

--- src/rot13/rot13/ROT13.cs
+++ src/rot13/rot13/ROT13.cs
@@ -6,11 +6,13 @@ namespace rot13
     {
         // Source: https://en.wikipedia.org/wiki/ROT13
         private const string INPUT = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        private const string OUTPUT = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
         
         
         public static string EncryptDecrypt(string text)
         {
-            return text;
+            var transformed = new List<char>();
+            if (transformed == null) throw new ArgumentNullException(nameof(transformed));
         }
     }
 }
