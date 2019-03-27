e58c13834e67c856d6a824eedd2c74b4e5892d72
Ralf Westphal <info@ralfw.de>
2019-03-27 10:14:10 +0100

    <dp commit>

--- src/rot13/rot13/FilesystemProvider.cs
+++ src/rot13/rot13/FilesystemProvider.cs
@@ -2,29 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace rot13
 {
     internal class FilesystemProvider
     {
-        public IEnumerable<string> EnumerateSourcefile(IEnumerable<string> sources)
-        {
+        public IEnumerable<string> EnumerateSourcefile(IEnumerable<string> sources) {
             var allFilenames = EnumerateAllFiles(sources);
             return SelectRelevantFiles(allFilenames);
         }
 
+        private IEnumerable<string> EnumerateAllFiles(IEnumerable<string> sources) {
+            return sources.SelectMany(EnumerateSource);
+
+            IEnumerable<string> EnumerateSource(string source) {
+                if (File.Exists(source)) return new[] {source};
+                if (Directory.Exists(source) is false) return new string[0];
+                return Directory.GetFiles(source, "*.*", SearchOption.AllDirectories);
+            }
+
+        }
+
         private IEnumerable<string> SelectRelevantFiles(IEnumerable<string> filenames)
-        {
-            throw new NotImplementedException();
-        }
+            => filenames.Where(fn => fn.ToLower().EndsWith(".txt") || fn.ToLower().EndsWith(".md"));
 
-        private IEnumerable<string> EnumerateAllFiles(IEnumerable<string> sources)
-        {
-            throw new NotImplementedException();
-        }
-
+        
         public string LoadText(string filename) => File.ReadAllText(filename);
 
+        
         public void  Store(string filename, string text)
         {
             // store
