deaf8384cf98c9e12fc75d90f5f1f386c1b8aa25
Ralf Westphal <info@ralfw.de>
2019-03-27 10:06:10 +0100

    <dp commit>

--- src/rot13/rot13/FilesystemProvider.cs
+++ src/rot13/rot13/FilesystemProvider.cs
@@ -1,7 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace rot13
 {
     internal class FilesystemProvider
     {
-        
+        public IEnumerable<string> EnumerateSourcefile(IEnumerable<string> sources)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
