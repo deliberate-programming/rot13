22ccb1c128f4f3cda550d8b778cb071b83e6c732
Ralf Westphal <info@ralfw.de>
2019-03-27 09:40:10 +0100

    <dp commit>

--- src/rot13/rot13.tests/Acceptance_tests.cs
+++ src/rot13/rot13.tests/Acceptance_tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Xunit;
 
 namespace rot13.tests
@@ -8,7 +9,19 @@ namespace rot13.tests
         [Fact]
         public void Encrypt()
         {
-            Assert.True(true);
+            CopyFiles("samples/unencrypted", "testencrypt");
+        }
+
+
+        static void CopyFiles(string sourcePath, string destinationPath)
+        {
+            Directory.Delete(destinationPath);
+            
+            var filenames = Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories);
+            foreach (var fn in filenames) {
+                var destFilename = fn.Replace(sourcePath, destinationPath);
+                File.Copy(fn, destFilename);
+            }
         }
     }
 }
