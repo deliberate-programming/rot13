a88bfb5bdaae4ca8c931992cc1c3a5af62d65432
Ralf Westphal <info@ralfw.de>
2019-03-27 10:34:10 +0100

    <dp commit>

--- src/rot13/rot13/Config.cs
+++ src/rot13/rot13/Config.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace rot13
 {
@@ -13,15 +15,27 @@ namespace rot13
         }
         
         
-        public Config(string[] args)
-        {
+        public Config(string[] args) {
             _args = args;
         }
 
 
-        (Commands Command, IEnumerable<string> Sources) Parse()
-        {
-            
+        (Commands Command, IEnumerable<string> Sources) Parse() {
+            return (ParseCommand(), ParseSources());
+
+            Commands ParseCommand() {
+                switch (_args[0].ToLower()) {
+                    case "encrypt":
+                    case "e":
+                        return Commands.Encrypt;
+                    case "dencrypt":
+                    case "d":
+                        return Commands.Decrypt;
+                }
+                throw new InvalidOperationException($"Unknown command: {_args[0]}!");
+            }
+
+            IEnumerable<string> ParseSources() => _args.Skip(1);
         }
     }
 }
