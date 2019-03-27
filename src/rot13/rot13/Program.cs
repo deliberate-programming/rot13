de331ba41447a2385e39b9c0929528c976854058
Ralf Westphal <info@ralfw.de>
2019-03-27 10:44:10 +0100

    <dp commit>

--- src/rot13/rot13/Program.cs
+++ src/rot13/rot13/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace rot13
+﻿namespace rot13
 {
     internal class Program
     {
@@ -13,33 +11,6 @@ namespace rot13
             var app = new App(config, requestHandler, display);
             
             app.Run();
-        }
-    }
-
-    class App
-    {
-        private readonly Config _config;
-        private readonly RequestHandler _requestHandler;
-        private readonly Display _display;
-
-        public App(Config config, RequestHandler requestHandler, Display display) {
-            _config = config;
-            _requestHandler = requestHandler;
-            _display = display;
-
-            _requestHandler.OnFileProcessed += _display.LogFileProcessed;
-        }
-
-        public void Run() {
-            var command = _config.Parse();
-            switch (command.Command) {
-                case Config.Commands.Encrypt:
-                    var n = _requestHandler.Encrypt(command.Sources);
-                    _display.LogNumberOfFilesProcessed(n);
-                    break;
-                default:
-                    throw new NotImplementedException();
-            }
         }
     }
 }
